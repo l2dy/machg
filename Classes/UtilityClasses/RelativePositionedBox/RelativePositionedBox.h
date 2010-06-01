8aee361cd7bcac47c05c325ff995cbf979cd3a3e
jfh <jason@jasonfharris.com>
2010-06-01 13:04:40 +0200

    - Add RelativePositionedBox to keep the position of a box relatively located at relative   x% and relative y% of the parent enclosing view.

---
+++ Classes/UtilityClasses/RelativePositionedBox/RelativePositionedBox.h
@@ -0,0 +1,16 @@
+//
+//  LocatedBox.h
+//  MacHg
+//
+//  Created by Jason Harris on 6/1/10.
+//  Copyright 2010 Jason F Harris. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface RelativePositionedBox : NSBox
+{
+}
+
+@end
---
+++ Classes/UtilityClasses/RelativePositionedBox/RelativePositionedBox.m
@@ -0,0 +1,32 @@
+//
+//  LocatedBox.m
+//  MacHg
+//
+//  Created by Jason Harris on 6/1/10.
+//  Copyright 2010 Jason F Harris. All rights reserved.
+//
+
+#import "RelativePositionedBox.h"
+
+
+@implementation RelativePositionedBox
+
+
+- (void) viewWillDraw
+{
+	static CGFloat relativeXPosition = 0.35;
+	static CGFloat relativeYPosition = 0.7;
+
+	NSRect f = [self frame];
+	NSRect bounds = [[super superview] bounds];
+	f.origin.x = round((bounds.size.width  - f.size.width ) * relativeXPosition);
+	f.origin.y = round((bounds.size.height - f.size.height) * relativeYPosition);
+	if (f.origin.x < 0)
+		f.origin.x = 0;
+	if (f.origin.y > f.size.height)
+		f.origin.y = f.size.height;
+	[self setFrame:f];
+	[super viewWillDraw];
+}
+
+@end
