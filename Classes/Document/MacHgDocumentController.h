d75e226af52a63e96c78d2f0692b2b7d65f9d383
jfh <jason@jasonfharris.com>
2010-06-09 15:02:48 +0200

    - Add an NSDocumentController class so we can control the number of recent items.  We need   at least one, independent of any system setting.

---
+++ Classes/Document/MacHgDocumentController.h
@@ -0,0 +1,16 @@
+//
+//  MacHgDocumentController.h
+//  MacHg
+//
+//  Created by Jason Harris on 6/9/10.
+//  Copyright 2010 Jason F Harris. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MacHgDocumentController : NSDocumentController
+{
+}
+
+@end
---
+++ Classes/Document/MacHgDocumentController.m
@@ -0,0 +1,20 @@
+//
+//  MacHgDocumentController.m
+//  MacHg
+//
+//  Created by Jason Harris on 6/9/10.
+//  Copyright 2010 Jason F Harris. All rights reserved.
+//
+
+#import "MacHgDocumentController.h"
+
+
+@implementation MacHgDocumentController
+
+- (NSUInteger)maximumRecentDocumentCount
+{
+	NSUInteger recentCount = [super maximumRecentDocumentCount];
+	return (recentCount > 0) ? recentCount : 1;
+}
+
+@end
