e3f8b63a5ef711f43706cd42186d8684216dd774
jfh <jason@jasonfharris.com>
2012-01-15 03:21:30 +0100

    - listen for the files view web view being opened.

--- Classes/FilesView/FSViewer.m
+++ Classes/FilesView/FSViewer.m
@@ -82,6 +82,7 @@
 {
 	[self observe:kBrowserDisplayPreferencesChanged from:nil byCalling:@selector(reloadDataSin)];
 	[self observe:kDifferencesDisplayPreferencesChanged from:nil byCalling:@selector(regenerateDifferencesInWebview)];
+	[self observe:kConcertinaViewContentDidUncollapse	from:detailedDiffWebView byCalling:@selector(regenerateDifferencesInWebview)];
 	
 	[parentController setMyDocumentFromParent];	// Set up the parent's myDocument since the partent's awakeFromNib has not yet been called.
 	rootNodeInfo_ = nil;
